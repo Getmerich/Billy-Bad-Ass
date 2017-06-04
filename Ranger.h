4a230b924a5b5e504624158dd9ab8aeddb88f945
demarchet <demarchet@yahoo.com>
2017-06-04 00:16:44 -0400

    Creating two classes

---
+++ Ranger.h
@@ -0,0 +1,14 @@
+/* Ranger Class Role  */
+
+
+class Ranger{
+	
+	public:
+
+	int dmg = 12;
+	int hp = 45;
+	int def = 3;
+	int hit = 95;	
+
+
+};
---
+++ Warrior.h
@@ -0,0 +1,22 @@
+/* Warrior Class Role 
+
+TO DO (EVENTUALLY): 
+inventory,
+map to walk on,
+monster spawning,
+battle based on location,
+different race/class combinations,
+
+*/
+
+
+class Warrior {
+	
+	public:
+
+	int dmg = 15;
+	int hp = 50;
+	int def = 5;
+	int hit = 85;
+
+};
