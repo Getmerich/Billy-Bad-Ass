589e9b9a7093554db3ce7bdf038b4b3e020132e8
Scaredturtle <demarcam2012@yahoo.com>
2017-06-04 18:23:59 -0400

    Window Creation
    Should now create a blank window for the game, error finding SDLmain.lib however. Will look into that before continuing.

--- Combat Sim/main.cpp
+++ Combat Sim/main.cpp
@@ -11,17 +11,57 @@
 #include "Ranger.h"
 #include <SDL.h>
 
-
 using namespace std;
+
+const int gameh = 640;
+const int gamew = 400;
 
 int turn(int, int, int);
 
 int main()
 {
+	//initializes the window
+	SDL_Window* window = NULL;
+
+	//initializes the 2d image that will be on the screen
+	SDL_Surface* screen = NULL;
+
+	//SDL needs to be started, this is the statement to initialize and catch errors
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		cout << "SDL could not start! SDL Error: " << SDL_GetError() << endl;
+	}
+	else {
+		//This should create the window for the game
+		window = SDL_CreateWindow("Badass Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, gamew, gameh, SDL_WINDOW_SHOWN);
+
+		//Of course error checking
+		if (window == NULL) {
+			cout << "Window could not be created! Error: " << SDL_GetError() << endl;
+		}
+		else {
+			//now we create the image to be created to the screen, for now just a color
+			screen = SDL_GetWindowSurface(window);
+			SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF));
+
+			//now push the new image to the screen
+			SDL_UpdateWindowSurface(window);
+
+			//add a delay so we can see the window
+			SDL_Delay(2000);
+		}
+	}
+	
+	//Close the window
+	SDL_DestroyWindow(window);
+
+	//Close SDL
+	SDL_Quit();
+
+	//commenting out the combat portion of the code until needed (maybe should make a class for combat functions
+	/*
 	Warrior Billy;
 	Skeleton Baddie;
-
-
+	
 	int eroll = 0, sroll = 0, hroll = 0;
 	int die = 0;
 	int i = 0;
@@ -54,8 +94,11 @@ int main()
 		cout << "Keep fighting?(y/n)    ";
 	
 		cin >> flag;
-	} while (Billy.hp > 0 && Baddie.hp > 0 && (flag == 'y' || flag == 'Y'));
-	std::system("pause");
+	} while (Billy.hp > 0 && Baddie.hp > 0 && (flag == 'y' || flag == 'Y')); */
+
+	cout << "Hope everything went well!!" << endl;
+
+	system("pause");
 
 	return 0;
 }
